c7419bbdde1fdb4357a9b3c4bf9d88b3c3b7d308
loic.porte <loic.porte@bibabox.fr>
2014-04-13 14:16:22 +0200

    First commit

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BinFile=/usr/bin/sshgithub
+ConfFile=/etc/sshgit.ini
+
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+cd $(dirname $0)
+
+cp sshgithub.py $BinFile
+chown root:root $BinFile
+chmod 500 $BinFile
+
+[ -e $ConfFile ] || cp config.ini $ConfFile
+chown root:root $ConfFile
+chmod 600 $ConfFile
---
+++ sshgithub.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+__author__ = 'bewiwi'
+
+import ConfigParser
+import sys
+import syslog
+import urllib2
+import os
+import stat
+
+class SSHGithub():
+
+    def __init__(self):
+        configpath='/etc/sshgit.ini'
+        configpath='sshgit.ini'
+        if not os.path.isfile(configpath):
+            raise Exception('Config file not found : '+configpath)
+        st = os.stat(configpath)
+        #Other write access
+        if bool(st.st_mode & stat.S_IWOTH):
+            raise Exception('Invalid right on the config file must be 0600')
+        if int(st.st_mode & stat.ST_UID) != 0 or int(st.st_mode & stat.ST_GID) != 0 :
+            raise Exception('Config file bad ownership must be root:root')
+
+        self.config = ConfigParser.ConfigParser()
+        self.config.read(configpath)
+        if not self.config.has_option('SERVER','url'):
+            raise Exception('No url parameter')
+        self.url = self.config.get('SERVER','url')
+
+    def getAuthorizedKey(self,SysUser):
+        users = self.getGitUserAuthorized(SysUser)
+        keys = []
+        for user in users:
+            keys=keys+self.getUserKeys(user)
+        return keys
+
+    def getGitUserAuthorized(self,SysUser):
+        if not self.config.has_option('USER',SysUser):
+            return []
+        return self.config.get('USER',SysUser).split(',')
+
+    def getUserKeys(self,user):
+        try:
+            userUrl = self.url+user+'.keys'
+            response = urllib2.urlopen(
+                userUrl
+            ).read()
+            strkeys = response.split('\n')
+            return strkeys
+        except urllib2.HTTPError as e:
+            syslog.syslog(syslog.LOG_ERR,'Error HTTP with '+userUrl)
+        except Exception as e:
+            syslog.syslog(syslog.LOG_ERR,'Unknown error '+str(e))
+        return []
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        raise Exception('Invalid argument number')
+    ssh_github = SSHGithub()
+    for key in ssh_github.getAuthorizedKey(sys.argv[1]):
+        print key
